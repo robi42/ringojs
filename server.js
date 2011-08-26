1d8ed341ef9ce11e4b9a14bf04325c335d19f41d
Robert Thurnher <r.thurnher@gmail.com>
2011-08-26 18:56:19 +0200

    Restrict env output.

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+var env = require('ringo/engine').properties;
+
 exports.app = function (req) {
     return {
         status: 200,
         headers: {"Content-Type": "text/plain"},
         body: [
             "Hello, shiny Ringo world!", "\n\n",
-            "Here's my env:", "\n",
-            JSON.stringify(require('ringo/engine').properties)
+            "Here's my home:", "\n",
+            env['ringo.home'], "\n",
+            "I'm running on:", "\n",
+            env['java.runtime.name']
         ]
     };
 };
